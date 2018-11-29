40b65981b346fa36d61daab40f2a25d1cf10803b
ThibCL <thibault.cabourg@gmail.com>
2018-11-29 18:30:29 +0100

    à deux doigts du suicide

--- migrations/env.py
+++ migrations/env.py
@@ -3,7 +3,7 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 import logging
-import os
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -27,7 +27,7 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
+
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
