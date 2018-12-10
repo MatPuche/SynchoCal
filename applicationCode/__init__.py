f7fb51f9d012ad439bd4ddf798fbf9493d9776c3
MatPuche <mathilde-puche@hotmail.com>
2018-12-10 12:05:00 +0100

    Creation des token fonctionne

--- applicationCode/__init__.py
+++ applicationCode/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 from . import db
 from . import auth
 from . import page_principale
-from . import page_accueil
 
 
 def create_app(test_config=None):
@@ -34,7 +33,8 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
     app.register_blueprint(page_principale.bp)
     app.add_url_rule('/', endpoint='page_principale')
-    app.register_blueprint(page_accueil.bp)
+
 
     return app
+
 app=create_app()
