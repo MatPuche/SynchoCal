ea7a2c08de1008d8663afac795aa83cabbff41a7
MatPuche <mathilde-puche@hotmail.com>
2018-12-13 17:38:48 +0100

    Réglatation des problèmes de services

--- applicationCode/with_calendar.py
+++ applicationCode/with_calendar.py
@@ -1,27 +1,23 @@
 from __future__ import print_function
-import json
-import datetime
-from googleapiclient.discovery import build
-from httplib2 import Http
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
 from oauth2client import file, client, tools
 import flask
-import argparse
+from httplib2 import Http
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = 'https://www.googleapis.com/auth/calendar'
+
+
+API_SERVICE_NAME = 'calendar'
+API_VERSION = 'v3'
 
 #   Afin de créer le fichier token.json, il faut exécuter
 def connection_cal():
-    store = file.Storage('token.json')
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
 
-        parser = argparse.ArgumentParser(parents=[tools.argparser])
-        flags = parser.parse_args(args=[])
+    credentials = google.oauth2.credentials.Credentials(
+         **flask.session['credentials'])
 
-        creds = tools.run_flow(flow, store, flags)
-    service = build('calendar', 'v3', credentials = creds)
-
+    service = googleapiclient.discovery.build(
+         API_SERVICE_NAME, API_VERSION, credentials=credentials)
 
     return service
