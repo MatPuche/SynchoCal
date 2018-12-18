34ba669ff99e5d0f34987d8f741f326e10e40eb7
MatPuche <mathilde-puche@hotmail.com>
2018-12-18 17:09:57 +0100

    zjkfvdsl

--- applicationCode/__init__.py
+++ applicationCode/__init__.py
@@ -1,9 +1,11 @@
-import os
+#Ce fichier contient le constructeur de l’application (fonction create_app) et indique à Python que le répertoire applicationCode doit être traité comme un package.
+#C’est à partir de ce fichier que tout est initialisé et que le lancement de l’application est rendu possible.
 
 from flask import Flask
 from . import db
 from . import auth
 from . import page_principale
+import os
 
 
 def create_app(test_config=None):
@@ -16,24 +18,25 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
+    # assure que le dossier de l'application existe
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
+    # récupère les données dans la base de données
     db.init_app(app)
 
+    # ajoute les Blueprint à l'application
     app.register_blueprint(auth.bp)
     app.register_blueprint(page_principale.bp)
-    app.add_url_rule('/', endpoint='page_principale')
 
+    #La vue de la page principale est reliée à l'index principal "/"
+    app.add_url_rule('/', endpoint='page_principale')
 
     return app
 
--- applicationCode/auth.py
+++ applicationCode/auth.py
@@ -22,20 +22,19 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 #creation d'un blueprint nommé "auth", associé à l'URL /auth
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-# This variable specifies the name of a file that contains the OAuth 2.0
-# information for this application, including its client_id and client_secret.
+# Cette variable spécifie le nom du fichier contenant les informations OAuth 2.0 pour l'application,
+# y compris son client_id et son client_secret.
 CLIENT_SECRETS_FILE = "client_secret.json"
 
-# This OAuth 2.0 access scope allows for full read/write access to the
-# authenticated user's account and requires requests to use an SSL connection.
+# Ce Scope d'accès OAuth 2.0 autorise un accès complet en lecture et éciture au compte Google authentifié.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+# caractéristiques de l'API de Google Calendar
 API_SERVICE_NAME = 'calendar'
 API_VERSION = 'v3'
 
 app = flask.Flask(__name__)
-# Note: A secret key is included in the sample so that it works.
-# If you use this code in your application, replace this with a truly secret
-# key. See http://flask.pocoo.org/docs/0.12/quickstart/#sessions.
+
 app.secret_key = 'ljgjhkgl'
 
 
@@ -114,23 +113,20 @@ def login():
     return render_template('login.html')
 
 
-
+# Page sur laquelle l'utilisateur est redirigée lorsqu'il se connecte à son compte
+# SynchroCal pour authentifier son compte Google.
 @bp.route('/authorize')
 def authorize():
-  # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+  # Créé une istance "flow" pour gérer les étapes d'octroi d'autorisations OAuth 2.0.
   flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
       CLIENT_SECRETS_FILE, scopes=SCOPES)
 
   flow.redirect_uri = flask.url_for('auth.oauth2callback', _external=True)
 
   authorization_url, state = flow.authorization_url(
-      # Enable offline access so that you can refresh an access token without
-      # re-prompting the user for permission. Recommended for web server apps.
       access_type='offline',
-      # Enable incremental authorization. Recommended as a best practice.
       include_granted_scopes='true')
 
-  # Store the state so the callback can verify the auth server response.
   flask.session['state'] = state
 
   return flask.redirect(authorization_url)
@@ -138,8 +134,7 @@ def authorize():
 
 @bp.route('/oauth2callback')
 def oauth2callback():
-  # Specify the state when creating the flow in the callback so that it can
-  # verified in the authorization server response.
+
   state = flask.session['state']
 
   flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
