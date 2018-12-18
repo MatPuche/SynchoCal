e9179ba72d21ed022a4d0c2856542bcd4319ce54
ThibCL <thibault.cabourg@gmail.com>
2018-12-18 14:15:03 +0100

    on remet tous

---
+++ migrations/versions/008624972f5c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 008624972f5c
+Revises: 60c587609b3b
+Create Date: 2018-11-29 19:21:44.167982
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '008624972f5c'
+down_revision = '60c587609b3b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
